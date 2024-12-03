366883a3af37779be97fd4454c94a2615f987c9a
Lourens Schep <lourensschep@gmail.com>
2024-12-03 12:37:09 +0100

    Skip underscore keys and support objects in bulk editing values

--- packages/dataviews/src/components/dataform/index.tsx
+++ packages/dataviews/src/components/dataform/index.tsx
@@ -14,6 +14,7 @@ import { MIXED_VALUE } from '../../constants';
 
 /**
  * Loops through the list of data items and returns an object with the intersecting ( same ) key and values.
+ * Skips keys that start with an underscore.
  *
  * @param data list of items.
  */
@@ -21,14 +22,26 @@ function getIntersectingValues< Item extends object >( data: Item[] ): Item {
 	const intersectingValues = {} as Item;
 	const keys = Object.keys( data[ 0 ] ) as Array< keyof Item >;
 	for ( const key of keys ) {
+		// Skip keys that start with underscore.
+		if ( key.toString().startsWith( '_' ) ) {
+			continue;
+		}
 		const [ firstRecord, ...remainingRecords ] = data;
-		const intersects = remainingRecords.every( ( item ) => {
-			return item[ key ] === firstRecord[ key ];
-		} );
-		if ( intersects ) {
-			intersectingValues[ key ] = firstRecord[ key ];
+
+		if ( typeof firstRecord[ key ] === 'object' ) {
+			// Traverse through nested objects.
+			intersectingValues[ key ] = getIntersectingValues(
+				data.map( ( item ) => item[ key ] as object )
+			) as Item[ keyof Item ];
 		} else {
-			intersectingValues[ key ] = MIXED_VALUE as Item[ keyof Item ];
+			const intersects = remainingRecords.every( ( item ) => {
+				return item[ key ] === firstRecord[ key ];
+			} );
+			if ( intersects ) {
+				intersectingValues[ key ] = firstRecord[ key ];
+			} else {
+				intersectingValues[ key ] = MIXED_VALUE as Item[ keyof Item ];
+			}
 		}
 	}
 	return intersectingValues;

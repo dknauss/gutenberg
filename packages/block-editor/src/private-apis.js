0cc03def06e68a3739337e5209e96bd7fa69c8c9
akasunil <sdprajapati999@gmail.com>
2024-12-06 19:28:29 +0530

    Export useBlockElement instead of useBlockElement

--- packages/block-editor/src/private-apis.js
+++ packages/block-editor/src/private-apis.js
@@ -50,7 +50,7 @@ import useBlockDisplayTitle from './components/block-title/use-block-display-tit
 import TabbedSidebar from './components/tabbed-sidebar';
 import CommentIconSlotFill from './components/collab/block-comment-icon-slot';
 import CommentIconToolbarSlotFill from './components/collab/block-comment-icon-toolbar-slot';
-import { useBlockElement } from './components/block-list/use-block-props/use-block-refs';
+import { useBlockElementRef } from './components/block-list/use-block-props/use-block-refs';
 
 /**
  * Private @wordpress/block-editor APIs.
@@ -99,5 +99,5 @@ lock( privateApis, {
 	sectionRootClientIdKey,
 	CommentIconSlotFill,
 	CommentIconToolbarSlotFill,
-	useBlockElement,
+	useBlockElementRef,
 } );
